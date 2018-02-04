d52b6c62537eee91c6712c3f91a280baef03eddd
bymars <liubei447@gmail.com>
2018-02-04 13:18:03 +0800

    Login With Security Check (#11)
    Be aware of verify code and security check and ask answer through email

--- ms_main.py
+++ ms_main.py
@@ -4,7 +4,7 @@ from ms_email import MailReceiver
 from ms_alipay import AlipayAcct
 
 def main():
-  if len(sys.argv) != 5:
+  if len(sys.argv) != 7:
     usage()
     exit(1)
 
@@ -19,9 +19,29 @@ def main():
   receiver = MailReceiver(mail_user, mail_pass, proxy_addr='127.0.0.1', proxy_port=1080)
 
   acct = AlipayAcct(username, password)
-  acct.login()
-  (retcode, result) = acct.get_balance()
+  (retcode, result) = acct.login()
+  if retcode == 'login':
+    subject = ''.join(random.sample(string.ascii_letters, 32))
+    sender.send_mail(from_addr, to_addr, subject, attachment = result)
+    message = receiver.wait_mail_by_title(subject)
+    if 'done' in message['Subject']:
+      checkcode = message['Subject'].split(' - ')[-1]
+      (retcode, result) = acct.login_with_checkcode(checkcode)
+      if retcode == 'security':
+        subject = ''.join(random.sample(string.ascii_letters, 32))
+        sender.send_mail(from_addr, to_addr, subject, attachment = result)
+        message = receiver.wait_mail_by_title(subject)
+        if 'done' in message['Subject']:
+          security = message['Subject'].split(' - ')[-1]
+          acct.login_with_security(security)
+        else:
+          print('login failed')
+          exit(1)
+    else:
+      print('login failed')
+      exit(1)
 
+  (retcode, result) = acct.get_balance()
   if retcode == 'succ':
     subject = time.strftime('%Y-%m-%d',time.localtime(time.time())) + ' Balance:' + result
     sender.send_mail(from_addr, to_addr, subject)
@@ -33,9 +53,6 @@ def main():
       (retcode, result) = acct.get_balance()
       subject2 = time.strftime('%Y-%m-%d',time.localtime(time.time())) + ' Balance:' + result
       sender.send_mail(from_addr, to_addr, subject2)
-  elif retcode == 'login':
-    subject = ''.join(random.sample(string.ascii_letters, 32))
-    sender.send_mail(from_addr, to_addr, subject, attachment = result)
 
 def usage():
   print('python3 ms_main.py <account> <password> <email_user> <email_pass> <from_addr> <to_addr>')
